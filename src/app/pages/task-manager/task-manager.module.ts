47cafa1a5b2b10b6a6cbe5d195d0f99719e4427b
joseph achonu <josephachonu@gmail.com>
2021-10-02 06:57:18 +0100

    added task manager components

--- src/app/pages/task-manager/task-manager.module.ts
+++ src/app/pages/task-manager/task-manager.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AboutComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule
   ]
